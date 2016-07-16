5cb2e087226e22a73e8eda1be44b5a0645fc30a9
James Rudd <james.rudd@gmail.com>
2016-07-17 01:00:27 +1000

    Use Bash test and option to copy file to another server

--- import/sdm630-poll.sh
+++ import/sdm630-poll.sh
@@ -3,6 +3,7 @@
 SDM630=/opt/sdm630/sdm630
 DATADIR=/var/tmp
 LATESTDATA=ACTsdm630.txt
+#REMOTEDEST="user@server"
 RUNMAX=10  # Attempt this many times
 SLEEP=18s  # wait this long between attempts
 
@@ -15,27 +16,30 @@ LOGFILE=$DATADIR/sdm-poll-$NOW.log
 RUNCOUNT=0
 EXIT=1
 
-while [ $EXIT -ne 0 ] && [ $RUNCOUNT -lt $RUNMAX ]; do
+while [[ $EXIT -ne 0 ]] && [[ $RUNCOUNT -lt $RUNMAX ]]; do
 	# Loop
 	$SDM630 > $DATAFILE
 	EXIT=$?
 	
 	let RUNCOUNT=RUNCOUNT+1 
 	echo "$(date -I'seconds') Ran counter $RUNCOUNT times, $EXIT code" >> $LOGFILE
-	if [ $EXIT -eq 0 ] ; then
+	if [[ $EXIT -eq 0 ]] ; then
 		/bin/grep -Fq SUCCESS $DATAFILE
 		EXIT=$?
 	fi
-	if [ $EXIT -ne 0 ] ; then
+	if [[ $EXIT -ne 0 ]] ; then
 		/bin/rm $DATAFILE
 		/bin/sleep $SLEEP
 	fi
 done
 
-if [ $EXIT -eq 0 ] ; then
+if [[ $EXIT -eq 0 ]] ; then
 	#/bin/rm $DATADIR/$LATESTDATA
 	/bin/cp $DATAFILE $DATADIR/$LATESTDATA
 	#echo "$(date -I'seconds') SDM Ran correctly after $RUNCOUNT times and exited with $EXIT code" >> $LOGFILE
+	if [[ -n "$REMOTEDEST" ]] ; then
+		scp -q $DATADIR/$LATESTDATA $REMOTEDEST:$DATADIR/$LATESTDATA
+	fi
 	/bin/rm $LOGFILE
 
 else
